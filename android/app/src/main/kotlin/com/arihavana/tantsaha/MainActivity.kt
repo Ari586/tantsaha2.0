e94ea5394612c9ae608e4722d01c1b3fdc27700e
Ari586 <Ari586@users.noreply.github.com>
2026-01-02 22:47:35 +0000

    deploy: 1ed348160bb3886a3d6c6a366674466fc4e61d5f

--- android/app/src/main/kotlin/com/arihavana/tantsaha/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.arihavana.tantsaha
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- scripts/build_web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
-flutter build web
